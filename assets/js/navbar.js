477ab7e4a48325b45e78775634eacf61b0ba7ffd
halha <hammamalhakim773@gmail.com>
2020-08-31 09:08:32 +0700

    design v1

---
+++ assets/js/navbar.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+  $('nav .show').click(() => {
+    $('nav ul').css({
+      right: '0',
+    });
+  });
+
+  $('nav ul .hide').click(() => {
+    $('nav ul').css({
+      right: '-100%',
+    });
+  });
+
+  // Scroll function
+  $(window).scroll((e) => {
+    let scroll = $(window).scrollTop();
+    let screenHeight = window.innerHeight;
+
+    if (scroll > screenHeight * 0.1) {
+      $('nav').addClass('fixed');
+      $('nav ul li a').addClass('scroll');
+      $('nav .show').removeClass('black');
+      $('nav .hide').removeClass('black');
+      $('nav ul').removeClass('primary');
+
+      $('nav').css({
+        backgroundColor: 'var(--white)',
+        boxShadow: '0 6px 12px rgba(0, 0, 0, .1)',
+      });
+      $('nav ul li a').css({
+        color: 'var(--black)',
+      });
+      $('nav .logo').css({
+        color: 'var(--black)',
+        visibility: 'visible',
+      });
+    } else {
+      $('nav').removeClass('fixed');
+      $('nav ul li a').removeClass('scroll');
+      $('nav .show').addClass('black');
+      $('nav .hide').addClass('black');
+      $('nav ul').addClass('primary');
+
+      $('nav').css({
+        backgroundColor: 'unset',
+        boxShadow: 'unset',
+      });
+      $('nav .logo').css({
+        color: 'var(--black)',
+      });
+      $('nav ul li a').css({
+        color: 'var(--black)',
+      });
+    }
+  });
+});
